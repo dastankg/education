d3711641b67b4ee05ca72b627576743ed05d419e
dsazanov <astan12324@gmail.com>
2025-05-26 00:42:40 +0600

    Adjust JWT token lifetimes for access and refresh tokens.

--- config/settings.py
+++ config/settings.py
@@ -175,8 +175,8 @@ EMAIL_HOST_PASSWORD = os.getenv("SMTP_PASSWORD")
 
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(hours=1),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
+    "REFRESH_TOKEN_LIFETIME": timedelta(weeks=2),
 }
 
 REST_AUTH = {
