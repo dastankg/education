6723cb428afdc21eb29b6b1d8e9e40ef0c232097
Aiomedin Chorobaev <eliothab58@gmail.com>
2025-06-11 04:21:04 +0600

    fix

--- config/settings.py
+++ config/settings.py
@@ -168,7 +168,7 @@ EMAIL_HOST_PASSWORD = os.getenv("SMTP_PASSWORD")
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
-    "REFRESH_TOKEN_LIFETIME": timedelta(weeks=2),
+    "REFRESH_TOKEN_LIFETIME": timedelta(minutes=30),
 }
 
 REST_AUTH = {
